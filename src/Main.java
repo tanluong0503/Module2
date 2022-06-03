0a0ebcb2230dc19b8b5294d8fa44499945ac827f
tanluong0503 <nguyentanluong0503@gmail.com>
2022-06-03 15:00:21 +0700

    add file

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+        System.out.println("Hello fdsfsdfsdf!");
+    }
+}
---
+++ src/bai1/person.java
@@ -0,0 +1,7 @@
+package bai1;
+
+public class person {
+    public static void main(String[] args) {
+        System.out.println("luong dau tom");
+    }
+}
