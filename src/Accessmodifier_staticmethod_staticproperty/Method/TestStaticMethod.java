17c17e9d249172850aa9a75669d5ac56f7780b2a
tanluong0503 <nguyentanluong0503@gmail.com>
2022-07-20 15:08:22 +0700

    update

--- src/Accessmodifier_staticmethod_staticproperty/Method/TestStaticMethod.java
+++ src/Accessmodifier_staticmethod_staticproperty/Method/TestStaticMethod.java
@@ -5,9 +5,9 @@ public class TestStaticMethod {
         Student.change(); //calling change method
 
         //creating objects
-        Student s1 = new Student(111, "Trung dau dat");
-        Student s2 = new Student(222, "Tien dau tom ");
-        Student s3 = new Student(333, "Luong dau lanh");
+        Student s1 = new Student(1, "Trung dau dat");
+        Student s2 = new Student(2, "Tien dau tom ");
+        Student s3 = new Student(3, "Luong dau lanh");
 
         //calling display method
         s1.display();
