4000c8cc7d098982d8f0df4d1d900438a7313f1a
tanluong0503 <nguyentanluong0503@gmail.com>
2022-07-20 14:19:21 +0700

    update

--- src/Try_Catch/Try_Catch.java
+++ src/Try_Catch/Try_Catch.java
@@ -2,14 +2,19 @@ package Try_Catch;
 
 public class Try_Catch {
     public static void main(String[] args) {
-        int x = 0;
-        int y = 10;
-        try {
-            int num = x/y;
-            System.out.println("next-statement: Inside try block");
-        }catch (Exception ex){
-            System.out.println("Exception Occurred");
-        }
-        System.out.println("next-statement: Outside of try-catch");
+//        int x = 0;
+//        int y = 10;
+//        try {
+//            int num = x/y;
+//            System.out.println("next-statement: Inside try block");
+//        }catch (Exception ex){
+//            System.out.println("Exception Occurred");
+//        }
+//        System.out.println("next-statement: Outside of try-catch");
+        String x = "   CodeGym_Viet_Nam    ";
+        x = x.trim();
+        int index = x.indexOf("_");
+        String y = x.substring(0, index);
+        System.out.println(y);
     }
 }
